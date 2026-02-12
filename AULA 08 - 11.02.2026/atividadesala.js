33662bf19824688e9a5aed3273b57f5c6f6d29a7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-11 21:02:52 -0300

    Comecei o ex

--- AULA 08 - 11.02.2026/atividadesala.js
+++ AULA 08 - 11.02.2026/atividadesala.js
@@ -3,9 +3,17 @@
 Construa uma lista de compras:
 
 - Adicione um campo para o usuário inserir um item e um botão para "Salvar".
-- Quando o usuário clicar no botão "Salvar", armaene o item no localStorage;
+- Quando o usuário clicar no botão "Salvar", armazene o item no localStorage;
 - Insira um botão "Visualizar" para exibir na tela todos os itens da lista de compras que estão armazenados no localStorage;
 - Insira também um botão "Limpar" para apagar todos os itens armazenados no localStorage.
 */
 
-let inserir = getElementbyId
+function salvaritem() {
+    let inserir = document.getElementById(inserir)
+    localStorage.setItem(inserir)
+}
+
+
+let visualizar = document.getElementById(visualizar)
+
+console.log(salvaritem())
